79b2da1d6acbb6c498b63c55a532ee8f44909a8f
Taylor Cornelius <tcorneliusart@gmail.com>
2026-03-07 21:44:29 -0500

    increase mobile particle size range to 0.35-0.95px

--- src/components/DiceArena.jsx
+++ src/components/DiceArena.jsx
@@ -30,7 +30,7 @@ function makeParticles(W, H) {
     y: Math.random() * H,
     vx: (Math.random() - 0.5) * 0.5,
     vy: (Math.random() - 0.5) * 0.5,
-    size: mobile ? Math.random() * 0.4 + 0.2 : Math.random() * 1.5 + 0.5,
+    size: mobile ? Math.random() * 0.6 + 0.35 : Math.random() * 1.5 + 0.5,
     baseOpacity: mobile ? Math.random() * 0.1 + 0.9 : Math.random() * 0.3 + 0.6,
     opacity: 0,
     color: pickColor(),
